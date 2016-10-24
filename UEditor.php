b054544ff096e9ae04cb5291776eb96cce2f46fb
xutl <79109681@qq.com>
2016-10-24 17:03:07 +0800

    init

---
+++ UEditor.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\summernote;
+
+use Yii;
+use yii\helpers\Json;
+use yii\helpers\Html;
+use yii\widgets\InputWidget;
+
+/**
+ * Class UEditor
+ * @package xutl\ueditor
+ */
+class UEditor extends InputWidget
+{
+
+    /**
+     * @var array the options for the Bootstrap date time picker JS plugin.
+     * Please refer to the Bootstrap date time picker plugin Web page for possible options.
+     * @see http://fex.baidu.com/ueditor/
+     */
+    public $clientOptions = [];
+
+    /**
+     * {@inheritDoc}
+     * @see \yii\base\Object::init()
+     */
+    public function init()
+    {
+        parent::init();
+        if (!isset ($this->options ['id'])) {
+            $this->options ['id'] = $this->getId();
+        }
+
+        $this->clientOptions = array_merge([
+            'autoHeightEnabled' => true,
+            'initialFrameWidth' => '100%',
+            'initialFrameHeight' => '300',
+            'lang' => Yii::$app->language == 'zh-CN' ? 'zh-cn' : 'en',
+        ], $this->clientOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        if ($this->hasModel()) {
+            echo Html::activeTextArea($this->model, $this->attribute, $this->options);
+        } else {
+            echo Html::textArea($this->name, $this->value, $this->options);
+        }
+        UEditorAsset::register($this->view);
+        $options = empty ($this->clientOptions) ? '' : Json::htmlEncode($this->clientOptions);
+        $this->view->registerJs("UE.getEditor(\"{$this->options['id']}\", {$options});");
+    }
+}
---
+++ UEditorAsset.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\summernote;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * Class UEditorAsset
+ * @package xutl\ueditor
+ */
+class UEditorAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/xutl/yii2-summernote--widget/assets';
+
+    public $js = [
+        'summernote.min.js',
+    ];
+
+    public $css = [
+        'summernote.css',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
